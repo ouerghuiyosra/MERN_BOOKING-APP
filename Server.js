53841d06b6ce5f3535a2e3a95a913e6794c48ae4
ouerghui yosra <ouerghuiyosra9@.com>
2021-08-16 02:42:52 +0200

    maker

--- Server.js
+++ Server.js
@@ -31,10 +31,10 @@ app.use('/api/booking',bookingRoutes);
 //Serve static assets in production
 if (process.env.NODE_ENV === "production") {
     //Set static folder
-    app.use(express.static("Client/build"));      
+    app.use(express.static("client/build"));      
   
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
   }
 
